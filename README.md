2d2b6113298846307bdf3d204348bd6f52cfa7fe
Marcos Dione <mdione@grulic.org.ar>
2017-04-03 23:55:24 +0200

    [!] fist import.

---
+++ README.md
@@ -0,0 +1,15 @@
+`dinant` is an attempt, like may others, to make regular expressions more
+readable, and, like many other, fails miserably... but we try anyways.
+
+You can find many examples in the source file, which includes unit tests to make
+sure we don't make things worse. Because it's implementation is currently very,
+very simple, it does not make any checks, so you can shoot you own foot and,
+because it doesn't even attempt to, it does not makes any optimizations, and
+resulting regexps can be more complex to read and less efficient. But the idea
+is that you would never see them again.
+
+What about the name? it'a nice town in België/Belgique/Belgien that I plan to
+visit some time. It also could mean 'dinning person' in French[1], which makes
+sense, as I wrote this during dinner.
+
+[1] but the real word is dîneur.
---
+++ dinant.py
@@ -0,0 +1,100 @@
+import re
+
+def _any(*args):
+    if len(args) == 0:
+        return '.'
+    elif len(args) == 1:
+        return '[%s]' % args[0]
+    else:
+        return '(?:%s)' % '|'.join(args)
+
+def capture(s):
+    return '(%s)' % s
+
+def one_or_more(s, greedy=True):
+    result = '(?:%s)+' % s
+    if greedy:
+        result += '?'
+    return result
+
+def zero_or_more(s, greedy=True):
+    result = '(?:%s)*' % s
+    if greedy:
+        result += '?'
+    return result
+
+def maybe(s, greedy=True):
+    result = '(?:%s)?' % s
+    if greedy:
+        result += '?'
+    return result
+
+def then(s):
+    return re.escape(s)
+
+bol = '^'
+eol = '$'
+
+def _not(s):
+    return '^'+s
+
+def exactly(n, s):
+    return "%s{%d}" % (s, n)
+
+def between(m, n, s, greedy=True):
+    result = "%s{%d,%d}" % (s, m, n)
+    if greedy:
+        result += '?'
+    return result
+
+def exactly(n, s):
+    return "%s{%d}" % (s, n)
+
+def test():
+    def ass(x, y):
+        try:
+            assert x == y
+        except AssertionError:
+            print("%r != %r" % (x, y))
+            raise
+
+    ass(then('a'), 'a')
+    ass(then('[]'), '\[\]')
+
+    ass(re.compile(capture(_any('a-z'))).match('abc').groups(), ('a', ))
+    ass(re.compile(capture(zero_or_more(_any('a-z')))).match('abc').groups(), ('abc', ))
+    ass(re.compile(capture(zero_or_more(_any('a-z')))).match('').groups(), ('', ))
+    ass(re.compile(capture(one_or_more(_any('a-z')))).match(''), None)
+    ass(re.compile(capture(one_or_more(_any('a-z')))).match('a').groups(), ('a', ))
+
+    ass(re.compile(capture(_any('abc', 'def'))).match('abc').groups(), ('abc', ))
+    ass(re.compile(capture(_any('abc', 'def'))).match('def').groups(), ('def', ))
+
+    ass(re.compile(capture(_any())).match('def').groups(), ('d', ))
+
+    ass(re.compile(bol+capture(_any())+eol).match('def'), None)
+    ass(re.compile(bol+capture(_any('abc', 'def'))+eol).match('def').groups(), ('def', ))
+
+    ass(re.compile(bol+capture(maybe('foo')+_any('bar', 'baz'))+eol).match('bar').groups(), ('bar', ))
+    ass(re.compile(bol+capture(maybe('foo')+_any('bar', 'baz'))+eol).match('foobaz').groups(), ('foobaz', ))
+
+    # ass(capture(one_or_more(_any('a-z')))+zero_or_more(then('[')+capture(one_or_more(_any('a-z')))+then(']')), '')
+    # '((?:[a-z])+)(?:[((?:[a-z])+)])*'
+    #  ((?:[a-z])+)(?:[((?:[a-z])+)])*
+    #  ([a-z]+)(?:[([a-z]+)])*
+
+    foo = ( capture( one_or_more(_any('a-z')) ) +
+            zero_or_more(then('[') +
+                         capture( zero_or_more(_any('a-z')) ) +
+                         then(']')) )
+
+    ass(re.compile(foo).match('foo').groups(), ('foo', None))
+    ass(re.compile(foo).match('foo[]').groups(), ('foo', ''))
+    ass(re.compile(foo).match('foo[bar]').groups(), ('foo', 'bar'))
+    ass(re.compile(foo).match('foo[bar][]').groups(), ('foo', 'bar'))
+    # ass(re.compile(foo).match('foo[bar][baz]').groups(), ('foo', 'bar', 'baz'))
+    ass(re.compile(foo).match('foo[bar][baz][quux]').groups(),
+        ('foo', 'bar', 'baz', 'quux'))
+
+if __name__ == '__main__':
+    test()
