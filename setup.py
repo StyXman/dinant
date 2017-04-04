14d0431303ca056926f30fa86c4ccd69d1c8a7d2
Marcos Dione <mdione@grulic.org.ar>
2017-04-04 19:43:48 +0200

    [+] setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+# (c) 2017 Marcos Dione <mdione@grulic.org.ar>
+# for licensing details see the file LICENSE.txt
+
+from distutils.core import setup
+
+setup(
+    name ='dinant',
+    version = '0.1',
+    description = 'An attempt to make regular expressions more readable.',
+    author = 'Marcos Dione',
+    author_email = 'mdione@grulic.org.ar',
+    url = 'https://github.com/StyXman/dinant',
+    py_modules = [ 'dinant' ],
+    license = 'GPLv3',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        ],
+    )
